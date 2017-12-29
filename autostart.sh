7c038b74ba5e851019d4dc0d98148131fe1e3e7c
Jugraj Singh <jugraj.singh@paxcom.net>
2017-12-29 23:43:48 +0530

    start script corrected

--- autostart.sh
+++ autostart.sh
@@ -14,19 +14,18 @@ then
 fi
 
 # $1 Currency (Electroneum as ETN, Monero as XMR)
-# $2 CPU Threads
-# $3 ETN Pool
+# $2 ETN Pool
 
 
 if test $1 = ETN;
 then
-	if test $3 = easy;
+	if test $2 = easy;
 	then
-		screen torify ./xmrig -a cryptonight -o stratum+tcp://etn.easyhash.io:3630 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4 -t $2
+		screen torify ./xmrig -a cryptonight -o stratum+tcp://etn.easyhash.io:3630 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
 	else
-	screen torify ./xmrig -a cryptonight -o stratum+tcp://asiapool.electroneum.com:3333 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4 -t $2
+	screen torify ./xmrig -a cryptonight -o stratum+tcp://asiapool.electroneum.com:3333 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
 	fi
 elif test $1 = XMR;
 then
-	screen torify ./xmrig -o pool.supportxmr.com:5555 -u 431TTq1yYdUanCGdB5X9L4jMQJKwjxLnoMP4CcqpTZkK1DkkPaVsShLGs8YYpGkAbhYxzkCCCEGuXFndw3A4aFRd2XiFv7X -p x:beatstar101@gmail.com --nicehash -t $2
+	screen torify ./xmrig -o pool.supportxmr.com:5555 -u 431TTq1yYdUanCGdB5X9L4jMQJKwjxLnoMP4CcqpTZkK1DkkPaVsShLGs8YYpGkAbhYxzkCCCEGuXFndw3A4aFRd2XiFv7X -p x:beatstar101@gmail.com --nicehash
 fi
