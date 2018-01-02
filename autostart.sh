dcc09ceeb1d1c8cbaba388670e39c553c3cc6f9a
Jugraj Singh <jugraj.singh@paxcom.net>
2018-01-02 22:34:06 +0530

    re-enable tor for proxying

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install build-essential cmake libuv1-dev libmicrohttpd-dev
+sudo apt-get -y install build-essential cmake libuv1-dev libmicrohttpd-dev tor screen
 
 if ! [ -e "build" ];
 then
@@ -21,11 +21,11 @@ if test $1 = ETN;
 then
 	if test $2 = easy;
 	then
-		screen torsocks ./xmrig -a cryptonight -o stratum+tcp://etn.easyhash.io:3630 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
+		screen -d -m torsocks ./xmrig -a cryptonight -o stratum+tcp://etn.easyhash.io:3630 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4 -t 2
 	else
-		screen torsocks ./xmrig -a cryptonight -o stratum+tcp://asiapool.electroneum.com:3333 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4
+		screen -d -m torsocks ./xmrig -a cryptonight -o stratum+tcp://asiapool.electroneum.com:3333 -u etnkKPTknoHRSGXHakPfSSaDxaBnbAZ51gbdjsQ7eBN3Ru4AZVd49RLMFJVZDbodUTA7y9cmityG6EBszT1Dr4VR7RK7a71SMj -p 4 -t 2
 	fi
 elif test $1 = XMR;
 then
-	screen torsocks ./xmrig -o pool.supportxmr.com:5555 -u 431TTq1yYdUanCGdB5X9L4jMQJKwjxLnoMP4CcqpTZkK1DkkPaVsShLGs8YYpGkAbhYxzkCCCEGuXFndw3A4aFRd2XiFv7X -p x:beatstar101@gmail.com --nicehash
+	screen -d -m torsocks ./xmrig -o pool.supportxmr.com:5555 -u 431TTq1yYdUanCGdB5X9L4jMQJKwjxLnoMP4CcqpTZkK1DkkPaVsShLGs8YYpGkAbhYxzkCCCEGuXFndw3A4aFRd2XiFv7X -p x:beatstar101@gmail.com --nicehash -t 2
 fi
